81923fc060f15d3925d82874c62dbcbef3e6f6c9
William S <will.step.205@gmail.com>
2023-06-11 16:47:47 -0700

    new div element added for testing purposes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+const page = (
+  <div>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Fun facts about React</h1>
+      <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+  </div>
+)
+
 function App() {
   return (
     <div className="App">
+      {page}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
