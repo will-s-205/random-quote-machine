2e69af34dbf2bda8d6b0be0cb773e453b943a2b1
William S <will.step.205@gmail.com>
2023-06-17 18:29:15 -0700

    removed unnecessary lines of code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import MainContent from './components/MainContent'
 
 export default function App() {
   return (
     <div id="quote-box">
-      <Header />
-      <MainContent />
-      <Footer />
+
     </div>
   );
 }
--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className='footer'>
-            <small>© 2023 William Step</small>
-        </footer>
-    )
-}
-{/* <div class="footer">by <a href="https://codepen.io/hezag/">hezag</a></div> */}
