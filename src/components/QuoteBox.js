dfdf167a5ee24a0f7ae44eb1def823a70d601e76
William S <will.step.205@gmail.com>
2023-06-17 20:08:09 -0700

    basic UI complete

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,8 +1,21 @@
+import double_quotes from '../images/double-quotes-svgrepo-com.svg'
+
 export default function QuoteBox() {
     return (
         <div>
             <div id="quote-box">
-                <p>quote-box</p>
+                <div id="text">
+                    <p><img src={double_quotes} className='double-quotes'></img> random quote</p>
+                </div>
+                <div id="author">- author</div>
+                <div>
+                    <a href="twitter.com/intent/tweet" id="tweet-quote">
+                    <button >Twi</button>
+                    </a>
+                    <button>FCC</button>
+                    <button>Git</button>
+                    <button id="new-quote">Q</button>
+                </div>
             </div>
         </div>
     )
