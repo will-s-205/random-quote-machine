428b1a1d1ce8c57bef42ad796d8dfb7f98535f12
William S <will.step.205@gmail.com>
2023-06-17 18:55:47 -0700

    CSS center major adjustments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import QuoteBox from './components/QuoteBox';
 
 export default function App() {
   return (
-    <div id="quote-box">
-
+    <div className="App">
+      <QuoteBox />
     </div>
   );
 }
