d9c2283a90acf01dc0cd6586f9f2a096e02f6023
William S <will.step.205@gmail.com>
2023-06-18 11:37:18 -0700

    handleColorTheme logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import QuoteBox from './components/QuoteBox';
 import colorVariations from './data/colorVariations';
+import { random } from 'lodash';
 
 export default function App() {
-  var changeThemeColor = colorVariations[Math.floor(Math.random()*colorVariations.length)];
+  // const changeThemeColor = colorVariations[Math.floor(Math.random()*colorVariations.length)];
+  // let changeThemeColor = random(0, colorVariations.length);
+  // const randomColor = random(0, colorVariations.length);
+  // const changeThemeColor = colorVariations[randomColor];
+  const changeThemeColor = colorVariations[random(0, colorVariations.length)];
+  console.log(changeThemeColor.title);
+
+
+
 
   return (
     <div className="App" style={{ backgroundColor: changeThemeColor.color }}>
