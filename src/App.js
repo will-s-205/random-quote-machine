82f4104f69e759c6fef49fed3a3fdfd6688f6aa7
William S <will.step.205@gmail.com>
2023-06-19 20:04:45 -0700

    redundant lines of code removed

--- src/App.js
+++ src/App.js
@@ -6,24 +6,16 @@ import { useState } from 'react';
 export default function App() {
   const [themeColor, setThemeColor] = useState('--backgound');
 
-  // const style = {
-  //   // '--backgound': themeColor
-  //   '--backgound': "#000"
-  // };
-
   const changeThemeColor = () => {
     const randomNum = Math.floor(Math.random() * colorVariations.length);
-    const { color } = colorVariations[randomNum];
+    let { color } = colorVariations[randomNum];
 
     setThemeColor(color);
     console.log("App color is "+colorVariations[randomNum].title)
   }
 
   return (
-    // <div id="App" style={{background:"#000"}}> // WORKS
-    // <div id="App" style={{background:themeColor}} onClick={changeThemeColor}>
     <div id="App" style={{background:themeColor}}>
-      {/* <QuoteBox /> */}
       <QuoteBox handleNewQuoteButtonClick={changeThemeColor}/>
     </div>
   );
